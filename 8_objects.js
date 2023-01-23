55657efbf4d68bfdc09d4f98a73c5f328482a97f
Karan Gupta <d22124440@mytudublin.ie>
2023-01-23 16:24:37 +0000

    wip

--- 8_objects.js
+++ 8_objects.js
@@ -17,3 +17,27 @@ console.log(firstObj["answer"]);
 console.log(firstObj.hobbies[2]);
 console.log(firstObj.beverage.night);
 console.log(firstObj.action());
+
+const vehicle = {
+  wheels: 4,
+  engine: function () {
+    return "Vrooooom!";
+  },
+};
+
+console.log(vehicle);
+
+const truck = Object.create(vehicle);
+console.log(vehicle);
+truck.doors = 2;
+console.log(truck);
+console.log(truck.wheels);
+console.log(truck.engine());
+
+const car = Object.create(vehicle);
+console.log(car);
+car.doors = 4;
+car.engine = function () {
+  return "Whoosh!";
+};
+console.log(car.engine());
